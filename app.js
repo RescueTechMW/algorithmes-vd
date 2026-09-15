f7378817b677efa78a25431f5eee8f4c5f4542c2
SemaineFamille <melanie.weil.81@gmail.com>
2026-09-15 10:51:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  * © 2026 – Tous droits réservés
  */
 
-console.log("APP VERSION 15-09-2026 10h35");
+console.log("APP VERSION 15-09-2026 10h45");
 
 // ⚠️ Gestion globale des erreurs
 window.addEventListener("error", function(e) {
@@ -850,8 +850,10 @@ const Publications = [
   revue: "Journal of Emergency Medicine",
   liens: [{titre: "📖 Publication principale",
   url: "https://research.dial.uclouvain.be/bitstreams/5d6ce922-1efe-41bb-aa83-401f81c979ce/download"},
-{titre: "🧪 Étude complémentaire",
+{titre: "🧪 Un essai prospectif randomisé, à double dummy, comparant la pression intraveineuse de kétamine à faible dose à une perfusion courte de kétamine à faible dose pour le traitement de la douleur aux urgences",
 url: "https://www.sciencedirect.com/science/article/abs/pii/S0735675717301717"}],
+  {titre: "🧪 Y a-t-il un rôle pour la kétamine à dose subdissociative intraveineuse administrée en complément des opioïdes ou comme agent unique pour la gestion de la douleur aiguë aux urgences ?",
+url: "https://https://www.sciencedirect.com/science/article/abs/pii/S0736467916305388"}],
   images:["images/Kétamine_1.png","images/Kétamine_2.png","images/Kétamine_3.png","images/Kétamine_4.png","images/Kétamine_5.png","images/Kétamine_6.png","images/Kétamine_7.png","images/Kétamine_8.png","images/Kétamine_9.png"],
   favori: false , access: ["PUBLIC"]},
  
