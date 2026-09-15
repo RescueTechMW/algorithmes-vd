70ba01266c7f0032043003b2558d12115a43075c
SemaineFamille <melanie.weil.81@gmail.com>
2026-09-15 10:04:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  * © 2026 – Tous droits réservés
  */
 
-console.log("APP VERSION 15-09-2026 09h55");
+console.log("APP VERSION 15-09-2026 10h05");
 
 // ⚠️ Gestion globale des erreurs
 window.addEventListener("error", function(e) {
